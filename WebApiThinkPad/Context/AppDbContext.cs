cd318559e24896edb07d4329ddd70a28403f19d8
Goruchie <marcosgualtero43253@gmail.com>
2025-04-01 19:48:11 -0300

    Add AppDbContext Class and set it up

---
+++ WebApiThinkPad/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiThinkPad.Models;
+
+namespace WebApiThinkPad.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<ThinkPad> ThinkPads { get; set; }
+    }
+}

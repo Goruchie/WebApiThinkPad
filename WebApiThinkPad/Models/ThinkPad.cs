9aa9ff2b1b48224f79f2e55bc669f6e422bcd0a6
Goruchie <marcosgualtero43253@gmail.com>
2025-04-01 17:47:47 -0300

    Create the ThinkPad Class and its properties

---
+++ WebApiThinkPad/Models/ThinkPad.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiThinkPad.Models
+{
+    public class ThinkPad
+    {
+        public int Id { get; set; }
+        public required string Model { get; set; }
+        public required decimal Price { get; set; }
+        public required int ReleaseYear {  get; set; }
+    }
+}

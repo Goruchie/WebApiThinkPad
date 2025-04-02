a0d57c0cb1bdb376baeec3b47f8f8876472a9752
Goruchie <marcosgualtero43253@gmail.com>
2025-04-01 23:33:59 -0300

    Register service for the connection

--- WebApiThinkPad/Program.cs
+++ WebApiThinkPad/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using WebApiThinkPad.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Create a variable for the connection string
-var stringConnection = builder.Configuration.GetConnectionString("Connection");
+var connectionString = builder.Configuration.GetConnectionString("Connection");
+//Register service for the connection
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
